00646f97c58d8777ce3976640e39db5754ec3019
somaratita <sohaina.maratita@gmail.com>
2019-03-06 04:59:59 -0800

    ex5 update

---
+++ exercises/exercise5/assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	
+
+	$(".scribble").hover(function() {
+		$("#hidden").toggle();
+	});
+
+});
+
+
+
+//$("#hidden").hover(function() {
+		//$(this).css("color", "#ca5066");
+	//}, function() {
+		//$(this).css("color", "transparent");
+		
+		//$(this).css("display", "none");
+	//});
