d374035e9577126f74158d2134825a04ef30248d
somaratita <sohaina.maratita@gmail.com>
2019-03-06 10:58:18 -0800

    big update ex5

--- exercises/exercise5/assets/js/main.js
+++ exercises/exercise5/assets/js/main.js
@@ -2,9 +2,40 @@ $(document).ready(function() {
 	
 
 	$(".scribble").hover(function() {
-		$("#hidden").toggle();
+		$(".hidden").toggle();
 	});
 
+	$(".scribble2").hover(function() {
+		$(".hidden2").toggle();
+	});
+
+	$(".scribble3").hover(function() {
+		$(".hidden3").toggle();
+	});
+
+	$(".scribble4").hover(function() {
+		$(".hidden4").toggle();
+	});
+
+	$(".scribble5").hover(function() {
+		$(".hidden5").toggle();
+	});
+
+	$(".scribble6").hover(function() {
+		$(".hidden6").toggle();
+	});
+
+	$(".scribble7").hover(function() {
+		$(".hidden7").toggle();
+	});
+
+	$(".scribble9").hover(function() {
+		$(".hidden9").toggle();
+	});
+
+	$(".scribble10").hover(function() {
+		$(".hidden10").toggle();
+	});
 });
 
 
---
+++ exercises/exercise5/assets/js/main2.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$(".scribble4").hover(function() {
+		$(".hidden4").toggle();
+	});
+});
+
